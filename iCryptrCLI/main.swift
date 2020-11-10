5b8444c12abb02ec73929d6b8c3a4890a1145a23
Reuben <4957964+reubn@users.noreply.github.com>
2020-11-10 23:01:56 +0000

    🔥

--- iCryptrCLI/main.swift
+++ iCryptrCLI/main.swift
@@ -10,33 +10,43 @@ import Foundation
 
 let arguments = Array(CommandLine.arguments[1...])
 var stderr = StandardErrorOutputStream()
+var isDir : ObjCBool = false
 
 if(arguments.count == 0) {print("umm where's the key?", to: &stderr); exit(1)}
 
 let key = arguments[0]
-let files = Array(arguments[1...])
+let paths = Array(arguments[1...])
 
-if(files.count == 0) {print("can't decrypt thin air mate", to: &stderr); exit(1)}
+if(paths.count == 0) {print("can't decrypt thin air mate", to: &stderr); exit(1)}
 
-func decrypt(_ path: String){
-    let url = URL(fileURLWithPath: path)
+func decrypt(_ url: URL){
     let (fileData, fileName) = decryptFile(url, key) ?? (nil, nil)
     
     if(fileData == nil) {print("decryption failure", url); return}
     
-    let outfile = url.deletingLastPathComponent().appendingPathComponent(fileName!)
-    print(outfile)
+    let outURL = url.deletingLastPathComponent().appendingPathComponent(fileName!)
     
     do {
-        try fileData!.write(to: outfile, options: .atomic)
-        print("written", outfile)
+        try fileData!.write(to: outURL, options: .atomic)
+        print("written", outURL)
     } catch {
-        print("file write error")
+        print("file write error", outURL)
     }
 }
 
-for file in files {
-    decrypt(file)
+for path in paths {
+    let url = URL(fileURLWithPath: path)
+    
+    if FileManager.default.fileExists(atPath: url.path, isDirectory: &isDir) {
+        if(isDir.boolValue){
+            if let contents = try? FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil) {
+                for file in contents {decrypt(file)}
+            } else {print("read error", url)}
+        } else {decrypt(url)}
+    } else {
+        print("read error", url)
+    }
+    
 }
 
 
